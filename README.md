2e78ff3ae638d68da295a196fe393abc6e961831
Jack Bicknell <jackbicknell@gmail.com>
2020-04-23 17:52:33 +0100

    added homework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# wpp-python-class-two
+# python-programming-web
+This is a repo for the homework and labs for Python Programming Web course with General Assembly
+
---
+++ unit-1-lab/starter-code/twitter_app.py
@@ -0,0 +1,7 @@
+# Create a variable `username` and set it to "Nike".
+
+
+# Create a variable `follower_count` to hold the followers count and set it to `800000`.
+
+
+# Make a `print` statement that says, `The number of followers for <username> is <follower_count>`.
